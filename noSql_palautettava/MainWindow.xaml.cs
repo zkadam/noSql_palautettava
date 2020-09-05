621bbae275893dae043f789be6592101a6c32711
zkadam <adam.zakar@student.careeria.fi>
2020-09-05 13:40:11 +0300

    tallennus dbn toteutettu, tarvii hiottelu ja paivamaara lisays

--- noSql_palautettava/MainWindow.xaml.cs
+++ noSql_palautettava/MainWindow.xaml.cs
@@ -36,7 +36,7 @@ namespace noSql_palautettava
             //await collection.InsertOneAsync(new BsonDocument("Name", "Jack"));
 
             //var list = await collection.Find(new BsonDocument("Etunimi", "Timo")).ToListAsync();
-            var list = collection.Find(new BsonDocument()).ToList();
+            var list =await collection.Find(new BsonDocument()).ToListAsync();
 
             foreach (var document in list)
             {
@@ -45,6 +45,26 @@ namespace noSql_palautettava
             }
         }
 
-   
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+
+            var client = new MongoClient("mongodb://localhost:27017");
+            var database = client.GetDatabase("henkilorekisteri");
+            var collection = database.GetCollection<BsonDocument>("henkilot");
+
+            //{ Etunimi: "Adam", Sukunimi: "Zakar", Osoite: "Testikatu 42", Postinumero: "06100", Sähköposti: "zkadam@vmillle.fi", JäsenyydenAlkuPvm: 24 - 04 - 2020  },
+
+            var document = new BsonDocument  {
+                { "Etunimi",  txt_etunimi.Text.ToString() },
+                { "Sukunimi", txt_sukumimi.Text.ToString()  },
+                { "Osoite",  txt_osoite.Text.ToString()  },
+                { "Postinumero",  txt_postinum.Text.ToString()  },
+                { "Sähköposti",  txt_sahkoposti.Text.ToString()  },
+                { "JäsenyydenAlkuPvm", 2020},
+               
+            };
+            collection.InsertOneAsync(document);
+        }
     }
 }
