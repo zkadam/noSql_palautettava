f192fceb3dc856e0cb0049922c98549247c2df6e
zkadam <adam.zakar@student.careeria.fi>
2020-09-07 14:21:23 +0300

    update onnistuu, vain poisto jäljellä

--- noSql_palautettava/MainWindow.xaml.cs
+++ noSql_palautettava/MainWindow.xaml.cs
@@ -25,8 +25,9 @@ namespace noSql_palautettava
         public MainWindow()
         {
             InitializeComponent();
-        }
+           
 
+        }
         //--------------------------------------------------------------------------------------------henkilö tulostus
         private async void Button_Click(object sender, RoutedEventArgs e)
         {
@@ -55,37 +56,64 @@ namespace noSql_palautettava
             }
         }
 
-        //-------------------------------------------------------------------------------------------henkilö lisäys
-        private void Button_Click_2(object sender, RoutedEventArgs e)
+        //-------------------------------------------------------------------------------------------henkilö lisäys/päivitys
+        private void btn_Click(object sender, RoutedEventArgs e)
         {
-
-            try
+            //--------------------------------------------------------------------------------------lisäys
+            if (txt_idresult.Text.Length < 0)
             {
-                var client = new MongoClient("mongodb://localhost:27017");
-                var database = client.GetDatabase("henkilorekisteri");
-                var collection = database.GetCollection<BsonDocument>("henkilot");
+                try
+                {
+                    var client = new MongoClient("mongodb://localhost:27017");
+                    var database = client.GetDatabase("henkilorekisteri");
+                    var collection = database.GetCollection<BsonDocument>("henkilot");
 
-                //{ Etunimi: "Adam", Sukunimi: "Zakar", Osoite: "Testikatu 42", Postinumero: "06100", Sähköposti: "zkadam@vmillle.fi", JäsenyydenAlkuPvm: 24 - 04 - 2020  },
+                    //{ Etunimi: "Adam", Sukunimi: "Zakar", Osoite: "Testikatu 42", Postinumero: "06100", Sähköposti: "zkadam@vmillle.fi", JäsenyydenAlkuPvm: 24 - 04 - 2020  },
 
-                var document = new BsonDocument  {
-                    { "Etunimi",  txt_etunimi.Text.ToString() },
-                    { "Sukunimi", txt_sukunimi.Text.ToString()  },
-                    { "Osoite",  txt_osoite.Text.ToString()  },
-                    { "Postinumero",  txt_postinum.Text.ToString()  },
-                    { "Sähköposti",  txt_sahkoposti.Text.ToString()  },
-                    { "JäsenyydenAlkuPvm", datePicker_pvm.SelectedDate},
-               
-                };
-                collection.InsertOneAsync(document);
+                    var document = new BsonDocument  {
+                        { "Etunimi",  txt_etunimi.Text.ToString() },
+                        { "Sukunimi", txt_sukunimi.Text.ToString()  },
+                        { "Osoite",  txt_osoite.Text.ToString()  },
+                        { "Postinumero",  txt_postinum.Text.ToString()  },
+                        { "Sähköposti",  txt_sahkoposti.Text.ToString()  },
+                        { "JäsenyydenAlkuPvm", datePicker_pvm.SelectedDate},
+
+                    };
+                    collection.InsertOneAsync(document);
+
+                }
+                catch (Exception)
+                {
+
+                    MessageBox.Show("Tallennuus epäonnistui");
+                }
 
             }
-            catch (Exception)
-            {
 
-                MessageBox.Show("Tallennuus epäonnistui");
+            //--------------------------------------------------------------------------------------päivitys
+
+            else
+            {
+                try
+                {
+                    var client = new MongoClient("mongodb://localhost:27017");
+                    var database = client.GetDatabase("henkilorekisteri");
+                    var collection = database.GetCollection<BsonDocument>("henkilot");
+                    var id = new ObjectId(txt_id.Text.ToString());
+                    var filter = Builders<BsonDocument>.Filter.Eq("_id", id);
+                    var update = Builders<BsonDocument>.Update.Set("Etunimi", txt_etunimi.Text.ToString()).Set("Sukunimi", txt_sukunimi.Text.ToString()).Set("Osoite", txt_osoite.Text.ToString()).Set("Postinumero", txt_postinum.Text.ToString()).Set("Sähköposti", txt_sahkoposti.Text.ToString()).Set("JäsenyydenAlkuPvm", datePicker_pvm.SelectedDate);
+                    collection.UpdateOneAsync(filter, update);
+
+                    Button_Click_1(sender,e);
+                }
+                catch (Exception)
+                {
+
+                    MessageBox.Show("Päivitys epäonnistui");
+                }
+
             }
         }
-
         //--------------------------------------------------------------------------------------------henkilö haku
         private async void Btn_id_Click(object sender, RoutedEventArgs e)
         {
@@ -109,7 +137,7 @@ namespace noSql_palautettava
                     txt_sahkoposti.Text = document["Sähköposti"].ToString();
                     DateTime dt = document["JäsenyydenAlkuPvm"].ToUniversalTime();
 
-                    //datePicker_pvm.SelectedDate = DateTime.Parse(document["JäsenyydenAlkuPvm"].ToString());
+                    
                     datePicker_pvm.SelectedDate = dt;
 
 
@@ -124,12 +152,18 @@ namespace noSql_palautettava
                 
         }
 
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
 
+            txt_idresult.Text = "";
+            txt_etunimi.Text = "";
+            txt_sukunimi.Text = "";
+            txt_osoite.Text = "";
+            txt_postinum.Text = "";
+            txt_sahkoposti.Text = "";
+            datePicker_pvm.SelectedDate = DateTime.Today;
 
-
-
-        //-------------------------henkilö päivitys
-
+        }
 
 
     }
